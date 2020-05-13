082bd31587c51b4cacac109e9164eb2099575246
sukumar paul <sukumar.paul@gmail.com>
2020-05-13 14:56:55 +0100

    Updated file append operation

--- File Operations/FileAppend - Text.py
+++ File Operations/FileAppend - Text.py
@@ -6,3 +6,4 @@ with open("original.txt","a") as tables:
     for i in range(2,13):
         for j in range(1,13):
             print("{1:>2} times {0} is {2}".format(i, j, i * j), file=tables)
+        print("-"*20, file=tables)
