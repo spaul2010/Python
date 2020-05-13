68827467e2c8ef4f030674ff678fb18db0f4527a
sukumar paul <sukumar.paul@gmail.com>
2020-05-13 14:54:19 +0100

    File append operation

---
+++ File Operations/FileAppend - Text.py
@@ -0,0 +1,8 @@
+# ------------------------------------------------------------------------------
+#    File Operations - Append Text File
+# ------------------------------------------------------------------------------
+
+with open("original.txt","a") as tables:
+    for i in range(2,13):
+        for j in range(1,13):
+            print("{1:>2} times {0} is {2}".format(i, j, i * j), file=tables)
